c81b938afc0c6fe7228b44db24aba440b9b91de7
adityak840 <khandelwal.aditya840@gmail.com>
2025-08-05 22:17:24 +0530

    fix: cors origin

--- index.ts
+++ index.ts
@@ -9,10 +9,9 @@ const connectedDevices: Map<string, { id: string; name?: string }> = new Map();
 
 const app: Express = express();
 
-// Enable CORS for local frontend
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: process.env.FRONTEND_URL,
     methods: ["GET", "POST"],
     credentials: true,
   })
@@ -23,7 +22,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: process.env.FRONTEND_URL,
     methods: ["GET", "POST"],
     credentials: true,
   },
